c475f80ceadfd911d4db5b2b705e41ca38b8176b
Peter <peterlenard.arnoza.cics@ust.edu.ph>
2024-05-07 13:51:39 +0800

    fixed pdf

--- generate.php
+++ generate.php
@@ -127,13 +127,13 @@ $dompdf->loadHtml('
 	</div>
 	');
 
-// (Optional) Setup the paper size and orientation
-$dompdf->setPaper('A4', 'landscape');
+// // (Optional) Setup the paper size and orientation
+// $dompdf->setPaper('A4', 'landscape');
 
-// Render the HTML as PDF
-$dompdf->render();
+// // Render the HTML as PDF
+// $dompdf->render();
 
-// Output the generated PDF to Browser
-$dompdf->stream();
+// // Output the generated PDF to Browser
+// $dompdf->stream();
 
 ?>
